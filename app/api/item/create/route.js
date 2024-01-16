aecc92c78b5e6409f1ab6710bc4d8b66ec902730
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-16 10:26:00 +0900

    * add POST item/create test.

---
+++ app/api/item/create/route.js
@@ -0,0 +1,8 @@
+// app/api/item/create/route.js
+
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  console.log(await request.json());
+  return NextResponse.json({ message: 'アイテム作成' });
+}
