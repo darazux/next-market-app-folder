3c79dd1a47421283d3697b597b9df69fb3e0cb29
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-17 11:38:38 +0900

    * add root page getAllItems. * add domain "res.cloudiry.com" in config.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,35 @@
 // app/page.js
 
-const ReadAllItems = () => {
+import Image from 'next/image';
+import Link from 'next/link';
+
+const getAllItems = async () => {
+  const api_url = process.env.NEXT_PUBLIC_API_URL;
+  const resp = await fetch(`${api_url}api/item/readall`, { cache: 'no-store' });
+  const jsonData = await resp.json();
+  return jsonData.allItems;
+};
+
+const ReadAllItems = async () => {
+  const allItems = await getAllItems();
   return (
     <div>
-      <h1>hello</h1>
+      {allItems.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div key={item._id}>
+            <h2>{Number(item.price).toLocaleString()}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
