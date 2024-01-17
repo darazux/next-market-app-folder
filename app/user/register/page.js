23166ff5dd88bc40f3f2761a772e2f0e282566e9
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-17 10:24:46 +0900

    * add user/register front page.

---
+++ app/user/register/page.js
@@ -0,0 +1,72 @@
+// app/user/register/page.js
+
+'use client';
+
+import { useState } from 'react';
+
+const Register = () => {
+  const api_url = process.env.NEXT_PUBLIC_API_URL;
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await fetch(`${api_url}api/user/register`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      const jsonData = await resp.json();
+      alert(jsonData.message);
+    } catch (error) {
+      console.log(error);
+      alert('ユーザー登録失敗');
+    }
+  };
+  return (
+    <div>
+      <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={userData.name}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="名前"
+          required
+        />
+        <input
+          value={userData.email}
+          onChange={handleChange}
+          type="text"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={userData.password}
+          onChange={handleChange}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
+        <button>登録</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
